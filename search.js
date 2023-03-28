f9c80174865f91631e0bb8685542caef12de174b
kaylaweldon <124632032+kaylaweldon@users.noreply.github.com>
2023-03-28 01:41:38 -0400

    Added updated zones/boros

--- search.js
+++ search.js
@@ -4,15 +4,33 @@ function searchZone() {
   var dropdown = document.getElementById("dropdown");
   var dropdownList = document.getElementById("dropdownlist");
   
+  /*
   	  var testArr = ["1", "2", "3", "4"];
   	  for (let i = 0; i < testArr.length; i++) {
 		const li = document.createElement('li');
         li.textContent = testArr[i];
         dropdownList.appendChild(li);
 	  }
+	  */
   
   // Create a new XMLHttpRequest object
   var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+	  if(this.readyState == 4 && this.status == 200) {
+		  const fileContents = this.responseText.split("\n");
+		  dropdownList.innerHTML = "";
+		  for (let i = 0; i < fileContents.length; i++) {
+			if (fileContents[i].toUpperCase().indexOf(filter) > -1) {
+			  const li = document.createElement('li');
+			  li.textContent = fileContents[i];
+			  dropdownList.appendChild(li);
+		    }
+		  }
+	  }
+  };
+  
+  xhttp.open("GET", "https://raw.githubusercontent.com/kaylaweldon/Visual-Analytics-For-Rideshare/main/zones2.txt", true);
+  xhttp.send();
 
   /*
   // Define the function to be called when the request completes
