13d0291072d6959147939b1a0d01f534323f6e2b
kaylaweldon <124632032+kaylaweldon@users.noreply.github.com>
2023-03-28 16:40:58 -0400

    updated search bar and changed matching result to red.

--- search.js
+++ search.js
@@ -1,64 +1,55 @@
 function searchZone() {
-  var input = document.getElementById("zone-search");
-  var filter = input.value.toUpperCase();
-  var dropdown = document.getElementById("dropdown");
-  var dropdownList = document.getElementById("dropdownlist");
-  
-  /*
-  	  var testArr = ["1", "2", "3", "4"];
-  	  for (let i = 0; i < testArr.length; i++) {
-		const li = document.createElement('li');
-        li.textContent = testArr[i];
-        dropdownList.appendChild(li);
-	  }
-	  */
-  
-  // Create a new XMLHttpRequest object
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-	  if(this.readyState == 4 && this.status == 200) {
-		  const fileContents = this.responseText.split("\n");
-		  dropdownList.innerHTML = "";
-		  for (let i = 0; i < fileContents.length; i++) {
-			if (fileContents[i].toUpperCase().indexOf(filter) > -1) {
-			  const li = document.createElement('li');
-			  li.textContent = fileContents[i];
-			  dropdownList.appendChild(li);
-		    }
-		  }
-	  }
-  };
-  
-  xhttp.open("GET", "https://raw.githubusercontent.com/kaylaweldon/Visual-Analytics-For-Rideshare/main/zones2.txt", true);
-  xhttp.send();
+	var input = document.getElementById("zone-search");
+	var filter = input.value.toUpperCase();
+	var dropdown = document.getElementById("dropdown");
+	var dropdownList = document.getElementById("dropdownlist");
+	var myGeojsonLayer = null;
 
-  /*
-  // Define the function to be called when the request completes
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      // Parse the text file content into an array of strings
-      var zones = this.responseText.split("\n");
-	
-	// Search for matching results in the array of strings
-      var results = "";
-      for (var i = 0; i < zones.length; i++) {
-        if (zones[i].toUpperCase().indexOf(filter) > -1) {
-          results += "<a href='#'>" + testArr[i] + "</a>";
-        }
-      }
+	// Create a new XMLHttpRequest object
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if(this.readyState == 4 && this.status == 200) {
+			const fileContents = this.responseText.split("\n");
+			const zones = fileContents.map(line => line.split(",")); // create array of arrays
+			dropdownList.innerHTML = "";
+			for (let i = 0; i < zones.length; i++) {
+				
+				if (zones[i][2].toUpperCase().indexOf(filter) > -1) {
+					const li = document.createElement('li');
+					li.textContent = zones[i][2];
+					dropdownList.appendChild(li);
 
-      // Update the dropdown with the matching results
-      dropdown.innerHTML = results;
+					li.addEventListener('click', function() {
 
-      // Show the dropdown
-      dropdown.style.display = "block";
-    }
-  };
+						const searchBox = document.getElementById('zone-search');
+						searchBox.value = this.textContent.trim();
 
-  // Open the XMLHttpRequest object and specify the URL of the text file
-  xhttp.open("GET", "C:\Users\kayla\Desktop\zones.txt", true);
+						const zoneId = zones[i][0]; // get the zone ID from the array
+						map = new google.maps.Map(document.getElementById("map"), {
+							zoom: 10,
+							center: { lat: 40.7128, lng: -74.0060 },
+						});
 
-  // Send the XMLHttpRequest object
-  xhttp.send();
-  */
+						// NOTE: This uses cross-domain XHR, and may not work on older browsers.
+						map.data.loadGeoJson('https://raw.githubusercontent.com/billiegiansante/kmlFiles/master/map.geojson');
+
+
+						map.data.setStyle(function(feature) {
+							var objectid = feature.getProperty('objectid');
+							var color = "green";
+							if (objectid == zoneId) {
+								color = "red";
+							}
+							return {
+								fillColor: color,
+								strokeWeight: 1
+							}
+						});
+					});
+				}	
+			}
+		}
+	};
+	xhttp.open("GET", "https://raw.githubusercontent.com/kaylaweldon/Visual-Analytics-For-Rideshare/main/zones2.txt", true);
+	xhttp.send();
 }
